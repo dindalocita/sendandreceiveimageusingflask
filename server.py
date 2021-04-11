4e773b53bba0c2bbf93646b80369e27e072ec0fd
dindalocita <80640044+dindalocita@users.noreply.github.com>
2021-04-11 22:00:53 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,15 @@ from flask import Flask, request, Response
 import jsonpickle
 import numpy as np
 import cv2
+import os
+
+
+UPLOAD_FOLDER = '/home/dindalocita/mysite'
+
 
 # Initialize the Flask application
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # route http posts to this method
@@ -17,10 +23,13 @@ def test():
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     # do some fancy processing here....
+    #simpan gambar didalam server dengan path '/home/dindalocita/mysite'
+    cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'] , 'citra.jpg'), img)
+
+
 
     # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-                }
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
 
@@ -28,4 +37,4 @@ def test():
 
 
 # start flask app
-#app.run(host="0.0.0.0", port=5000)
+# app.run(host="0.0.0.0", port=5000)
